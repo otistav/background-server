2ac588a6fd9bcc384513824d32c5559b4d9b18c6
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-23 18:18:44 +0200

    docker setup

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 module.exports = {
     connect: cb => {
-        mongoose.connect('mongodb://localhost:27017/test', cb);
+        mongoose.connect('mongodb://mongo_container:27017/test', cb);
     },
     
 }
