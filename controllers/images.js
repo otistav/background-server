d742b305da59deafd1d56f6dbdd69f443489d7ce
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-23 19:08:10 +0200

    fix#1

--- controllers/images.js
+++ controllers/images.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 exports.load = async (req, res, next) => {
   try {
     const image = await imageService.createImage(req.file.filename, req.body.user_id);
-    await axios.post('http://python:5000/image', { filename: req.file.filename })
+    await axios.post('http://localhost:5000/image', { filename: req.file.filename })
     res.send(image);
   } catch (error) {
     res.send(error);
