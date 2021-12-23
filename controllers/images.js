68448302984ff2b91cb7fdfc8fd25abbfa58c2fd
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-24 01:40:09 +0200

    image send

--- controllers/images.js
+++ controllers/images.js
@@ -5,6 +5,7 @@ exports.load = async (req, res, next) => {
   try {
     const image = await imageService.createImage(req.file.filename, req.body.user_id);
     await axios.post('http://python:5000/image', { filename: req.file.filename });
+    res.send(image);
   } catch (error) {
     console.log(error, 'error11')
     res.send(error);
