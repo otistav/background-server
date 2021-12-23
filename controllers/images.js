d5cbfe036da66a12e0026e8f9239e20a0022baca
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-24 01:11:18 +0200

    clean

--- controllers/images.js
+++ controllers/images.js
@@ -1,19 +1,10 @@
 const imageService = require('../services/imageService');
 const axios = require('axios');
-const fs = require('fs');
 
 exports.load = async (req, res, next) => {
   try {
     const image = await imageService.createImage(req.file.filename, req.body.user_id);
-    const data = await axios.post('http://python:5000/image', { filename: req.file.filename });
-    fs.writeFile(`../uploads/processed_${req.file.filename}`, data.data, err => {
-      if (err) {
-        console.log(err, 'error');
-        res.send(error);
-        return
-      }
-      res.send(image);
-    })
+    await axios.post('http://python:5000/image', { filename: req.file.filename });
   } catch (error) {
     console.log(error, 'error11')
     res.send(error);
