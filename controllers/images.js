aa1eb037850450101f0311a4d88d68585b391f5d
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-23 13:29:10 +0200

    filename request

--- controllers/images.js
+++ controllers/images.js
@@ -1,8 +1,10 @@
 const imageService = require('../services/imageService');
+const axios = require('axios');
 
 exports.load = async (req, res, next) => {
   try {
-    const image = await imageService.createImage(req.file.filename, req.body.user_id)
+    const image = await imageService.createImage(req.file.filename, req.body.user_id);
+    axios.post('http://localhost:5000/image', { filename: req.file.filename })
     res.send(image);
   } catch (error) {
     res.send(error);
