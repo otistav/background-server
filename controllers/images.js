e83c1665928339927be695e9d67a7f46e7f26b80
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-24 14:02:14 +0200

    fix

--- controllers/images.js
+++ controllers/images.js
@@ -7,7 +7,7 @@ exports.load = async (req, res, next) => {
     const processed_filename = `processed_${req.file.filename.split('.').slice(0, -1).join('.')}.png`
     const image = await imageService.createImage(req.file.filename, req.body.user_id, processed_filename);
     if (unprocessed_images.length === 0) {
-      axios.post('http://python:5000/image', { filename: req.file.filename });
+      axios.post(`http://${req.headers.host}:5000/image`, { filename: req.file.filename });
     }
     res.send(image);
   } catch (error) {
