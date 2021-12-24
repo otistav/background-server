b78a0e040b4020a54194f275db70d05abd3cdbc0
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-24 03:47:35 +0200

    get all images

--- routes/images.js
+++ routes/images.js
@@ -6,7 +6,7 @@ const Image = require('../db/models/image');
 router.post('/load', uploadMiddleware.single, imageController.load);
 
 router.get('/test', async (req, res, next) => {
-  res.send((await Image.find({})).length)
+  res.send(await Image.find({}))
 })
 
 router.get('/del', async (req, res, next) => {
