8c799d8aae30b0edd005842d532ff8c67bc3a61b
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-24 04:41:52 +0200

    image get

--- routes/images.js
+++ routes/images.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const imageController = require('../controllers/images');
 const uploadMiddleware = require('../middlewares/imgUpload');
 const Image = require('../db/models/image');
+const imageService = require('../services/imageService');
+
 router.post('/load', uploadMiddleware.single, imageController.load);
 
-router.get('/test', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   res.send(await Image.find({}))
 })
 
